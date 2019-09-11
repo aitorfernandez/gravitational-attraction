9577633c8c6a1e2649e87e237eac5863ead70844
Aitor Fernandez <aitor@qad.re>
2019-09-11 11:06:08 +0100

    add colors

--- gravitational-attraction.js
+++ gravitational-attraction.js
@@ -2,7 +2,35 @@ export default (p) => {
   const margin = 48
   const triangles = []
 
+  const red = [
+    '#db556e', '#cc4161', '#f791a7', '#e20b2b', '#9b2026', '#e08f76', '#ff93b5', '#ea798a', '#f4738f', '#d83a57', '#b70345', '#fcc7d7', '#dd6544', '#ea3e35', '#d35654', '#cc1631', '#db183f', '#a5181b'
+  ]
+
+  const orange = [
+    '#eacf9d', '#e2b171', '#f78b42', '#f4bf7f', '#f9e7b8', '#e08a62', '#f7a518', '#f4be9c', '#f7ba60', '#efd0a5', '#d8a654', '#edba68', '#d84704', '#fcedc2', '#f2d1a9', '#f29c6a', '#f9b970', '#f2b035'
+  ]
+
+  const blue = [
+    '#03afaf', '#0b86dd', '#6c9dcc', '#5d9bbf', '#02b9d6', '#2b7387', '#09889b', '#603ac9', '#051f87', '#b3f1f2', '#7e69d3', '#1821a0', '#7488db', '#b8aaef', '#00707a', '#17777c', '#112d77', '#8d72e5'
+  ]
+
+  const purple = [
+    '#8019c4', '#772ce0', '#9258dd', '#803bb5', '#9d55c6', '#4608af', '#9d7ae2', '#8b0ef2', '#5f13b5', '#6611d6', '#964dc6', '#660a91', '#7e22c9', '#893ded', '#3d0a84', '#7d54dd', '#6e3ead', '#bba1f4'
+  ]
+
+  const monochrome = [
+    '#878787', '#444444', '#969696', '#9e9e9e', '#8c8c8c', '#636363', '#aaaaaa', '#878787', '#727272', '#cccccc', '#686868', '#999999', '#666666', '#333333', '#a5a5a5', '#8e8e8e', '#515151', '#232323'
+  ]
+
+  const colors = [
+    red, orange, blue, purple, monochrome
+  ]
+
+  let hue
+
   function createTriangle() {
+    const color = p.random(hue)
+
     const initialVertex = p.round(p.random(60, 80))
     const vertex = {
       v1: initialVertex, v2: initialVertex, v3: initialVertex
@@ -51,6 +79,8 @@ export default (p) => {
     }
 
     function draw() {
+      p.fill(color)
+
       p.push()
       p.rotate(dir ? angle : angle * -1)
       p.translate(distance, 0)
@@ -75,6 +105,8 @@ export default (p) => {
   }
 
   function reset() {
+    hue = p.random(colors)
+
     triangles.length = 0
 
     for (let i = 0, len = 12; i < len; i += 1) {
@@ -95,7 +127,6 @@ export default (p) => {
     p.background('#111')
 
     p.noStroke()
-    p.fill(255, 100)
 
     p.push()
     p.translate(p.width / 2, p.height / 2)
